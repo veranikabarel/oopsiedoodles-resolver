d79f76f580b875f497722937b82e378990d4a0cb
veranikabarel <veranikabarel@gmail.com>
2023-08-27 21:35:43 +0200

    update readme and package.json

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Oopsiedoodles resolver
+
+Oopsiedoodles resolver is a delightful npm package that transforms those mundane error messages into a hilarious collection of chuckles and giggles. Tired of boring "404 Not Found" messages? With Oopsiedoodles resolver, you'll be tickled by witty and amusing error responses that bring a smile to your face even when things go awry. Spice up your app's error handling with a touch of humor, making both developers and users grin in the face of adversity. Who knew errors could be this much fun?
+
+## Table of contents
+
+- [Features](#features)
+- [Main](#main)
+- [Getting started](#getting-started)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Transform common client error codes into amusing messages.
+- Improve user experience with light-hearted error handling.
+- Easy integration into your existing error handling workflow.
+
+**Disclaimer**: Oopsiedoodles resolver is intended for entertainment purposes only and should not be used in critical applications where accurate error messages are crucial.
+
+## Main files
+
+```text
+dist/
+├── index.js
+└── index.d.ts 
+```
+
+## Getting started
+
+### Installation
+
+```shell
+npm install oopsiedoodles-resolver
+```
+
+### Usage
+
+```js
+import { translateErrorMessage } from 'oopsiedoodles-resolver';
+
+console.log(translateErrorMessage('Forbidden'));
+```
+
+## Contributing
+
+Contributions are always welcome! Feel free to open issues and pull requests for bug fixes, improvements, or new features. Please make sure to follow the existing code style and test your changes before submitting.
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+[⬆ back to top](#table-of-contents)
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 const translations: Record<string, string> = {
-  "Bad request.": "Whoopsie-daisy! Your request is a bit wonky.",
-  "Unauthorized access.": "Access denied! This is a VIP-only party.",
-  "Forbidden.": "You shall not pass! Forbidden territory ahead.",
-  "Page not found.": "Oops, this page took a wrong turn at Albuquerque.",
-  "Request timeout.": "Request tried to do the slow-motion moonwalk, timed out.",
-  "Too many requests.": "Slow down, cowboy! Server needs a breather.",
-  "Internal server error.": "Gremlins attacked our server, chaos ensued.",
-  "Bad gateway.": "Gateway to another dimension acting up, try again.",
-  "Service unavailable.": "Service took a vacation to Bermuda, back soon.",
-  "Gateway timeout.": "Gateway lost its way, taking a timeout.",
-  "Insufficient storage.": "Out of closet space, can't store your stuff.",
-  "Network authentication required.": "Network needs a secret handshake, authenticate yourself!",
+  "Bad request": "Whoopsie-daisy! Your request is a bit wonky.",
+  "Unauthorized access": "Access denied! This is a VIP-only party.",
+  Forbidden: "You shall not pass! Forbidden territory ahead.",
+  "Page not found": "Oops, this page took a wrong turn at Albuquerque.",
+  "Request timeout": "Request tried to do the slow-motion moonwalk, timed out.",
+  "Too many requests": "Slow down, cowboy! Server needs a breather.",
+  "Internal server error": "Gremlins attacked our server, chaos ensued.",
+  "Bad gateway": "Gateway to another dimension acting up, try again.",
+  "Service unavailable": "Service took a vacation to Bermuda, back soon.",
+  "Gateway timeout": "Gateway lost its way, taking a timeout.",
+  "Insufficient storage": "Out of closet space, can't store your stuff.",
+  "Network authentication required": "Network needs a secret handshake, authenticate yourself!",
 };
 
 export const translateErrorMessage = (originalErrorMessage: string) => {
